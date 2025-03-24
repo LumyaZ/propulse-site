394ff17c6a42c7692fe39a25550f58e7d614957b
thomas <cornu.thomas6@gmail.com>
2025-03-24 14:28:49 +0100

    fix: update vision component

--- src/components/Vision/Vision.jsx
+++ src/components/Vision/Vision.jsx
@@ -4,7 +4,7 @@ import './Vision.scss';
 const Vision = () => {
   return (
     <section id="vision" className="vision">
-      <p className="vision__text">Créons ensemble votre présence digitale</p>
+      <p className="vision__text">Construire une image solide, c’est préparer ta réussite</p>
     </section>
   );
 };
